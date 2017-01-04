2ba1830ec2dea0f4909f18bcb5743bf45abf18ad
Flavio <4411132+stribus@users.noreply.github.com>
2017-01-04 10:45:05 -0200

    inicial

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+		<form name="login" action="logar.php" method="POST">
+			Email : <input type="text" name="email" value="" size="10" /> <br>
+			Senha : <input type="password" name="senha" value="" size="10" />			
+			<br>				
+			<input type="submit" value="Ok" name="Ok" />
+			<input type="reset" value="Limpar" name="Limpar" />
+		</form>
+    </body>
+</html>
---
+++ logar.php
@@ -0,0 +1,38 @@
+<?
+
+include_once 'MsSQLConnection.php';
+
+$conexao = new MsSQLConnection();
+
+$query = 'select COD_SEG,SENHA from VW_LOGIN where MAIL = \''.$_REQUEST['email'].'\'';
+
+$resultset =  $conexao->buscar($query);
+
+$senha = $conexao->obterLinha( $resultset );
+
+if($senha){
+	if(!$senha['SENHA']){
+		$query = '
+	INSERT INTO [dbo].[SENHAS_SEGURADOS]
+           ([COD_SEG]
+           ,[SENHA])
+    VALUES
+           ('.$senha['COD_SEG'].'
+           ,\''.$_REQUEST['senha'].'\')';
+		$conexao->executar($query);
+	echo 'gravado senha '.$_REQUEST['senha'];
+	}elseif ($senha['SENHA']==$_REQUEST['senha']){
+		echo 'logado';
+	} else {
+		echo 'senha nao confere';
+	}
+	
+} else {
+	echo 'Usuario \''.$_REQUEST['email'].'\' nao cadastrado';
+	
+}
+	
+
+
+$conexao->fechar_conexao();
+?>
