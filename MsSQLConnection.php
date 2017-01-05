c722ffdbb625728d627ce82a080d9b2fdb206e84
Flavio <4411132+stribus@users.noreply.github.com>
2017-01-05 08:46:51 -0200

    Ajustes para deixar a classe de conexao mais proximo da classe do mySQL

--- MsSQLConnection.php
+++ MsSQLConnection.php
@@ -12,59 +12,153 @@
  * @author Flavio
  */
 class MsSQLConnection {
-	
+
 	protected $MSconn;
 	protected $serverName;
 	protected $connectionOptions;
-	protected $query;
+	public $query;
 
-
-	public function __construct() {
-		$this->serverName = "tcp:AGEME-01-PC";
-		$this->connectionOptions = array( "Database"=>"PORTAL_SEGURADO", "UID"=>"ageme", "PWD"=>"eme142536");
-	
+	public function __construct($nomeServidor = '', $opcoes = null) {
+		$this->serverName = ((empty($nomeServidor)) ? "tcp:AGEME-01-PC,49172" : $nomeServidor);
+		$this->connectionOptions = (($opcoes) ? $opcoes : array("Database" => "PORTAL_SEGURADO", "UID" => "ageme", "PWD" => "eme142536"));
+		$this->query = NULL;
 	}
-	
+
 	public function __destruct() {
+		$this->fechaQuery();
 		if ($this->MSconn)
 			$this->fechar_conexao();
 	}
-	
-	function conecta_MSSQL(){
-		$this->MSconn = sqlsrv_connect( $this->serverName, $this->connectionOptions );
-		if (!$this->MSconn)
-		{
+
+	function verifica() {
+		if (!$this->$MSconn) {
+			return FALSE;
+		} else {
+			return TRUE;
+		}
+	}
+
+	function conecta_MSSQL() {
+		$this->MSconn = sqlsrv_connect($this->serverName, $this->connectionOptions);
+		if (!$this->MSconn) {
 			var_dump(sqlsrv_errors());
 			die;
 		}
 	}
-	
-	function buscar($sql){
-		if (!$this->MSconn){
+
+	function buscar($sql, $params = array(), $options = array("Scrollable" => "buffered")) {
+		if (!$this->MSconn) {
 			$this->conecta_MSSQL();
 		}
-		return sqlsrv_query($this->MSconn,$sql);
+		if (empty($q)) {
+			die('e\' necessario uma "query".');
+		}
+		$this->fechaQuery();
+		$this->query = sqlsrv_query($this->MSconn, $sql, $params, $options) or $this->erro("ERRO NA CONSULTA!  <span style=\"display:none\">  $q </span>");
+		return $this->query;
 	}
-	
-	function executar($sql){
-		if (!$this->MSconn){
+
+	function faz($q = "") {
+		if (!$this->verifica()) {
+			$this->erro('e\' necessario conectar ao mysql antes.');
+		}
+		if (empty($q)) {
+			die('e\' necessario uma "query".');
+		}
+		$this->buscar($q);
+	}
+
+	private function fechaQuery() {
+		if ($this->query) {
+			$this->fechar_statement($this->query);
+			$this->query = NULL;
+		}
+	}
+
+	function executar($sql) {
+		if (!$this->MSconn) {
 			$this->conecta_MSSQL();
 		}
-		if (!$query){
+		$this->fechaQuery();
+		sqlsrv_query($this->MSconn, $sql);
+	}
+
+	function arrayx($a = null, $tipo = 'assoc') {
+		if (empty($a))
+			$a = $this->query;
+		switch (strtoupper($tipo)) {
+			case 'ASSOC':
+				$fc = SQLSRV_FETCH_ASSOC;
+				break;
+			case 'NUMERIC':
+				$fc = SQLSRV_FETCH_NUMERIC;
+				break;
+
+			default:
+				$fc = SQLSRV_FETCH_BOTH;
+				break;
 		}
-		sqlsrv_query($this->MSconn,$sql);
+		return sqlsrv_fetch_array($a, $fc);
 	}
-	
-	function obterLinha($resultSet){
-		return sqlsrv_fetch_array($resultSet);
+
+	function objeto($a = null) {
+		if (empty($a))
+			$a = $this->query;
+		return sqlsrv_fetch_object($a);
 	}
-	
-	function fechar_statement($stm){
-		sqlsrv_free_stmt( $stm );
+
+	function fechar_statement($stm) {
+		sqlsrv_free_stmt($stm);
 	}
-	
-	function fechar_conexao(){
-		sqlsrv_close( $this->MSconn );
+
+	function desconecta() {
+		$this->fechaQuery();
+		sqlsrv_close($this->MSconn);
 	}
-	
+
+	function registros($tipo = 'select') {
+		if (!$this->query) {
+			return 0;
+		} else {
+			switch ($tipo) {
+				case "delete":
+				case "update":
+					return qlsrv_rows_affected($this->query);
+					break;
+				case "select":
+				default:
+					return sqlsrv_query($this->query);
+					break;
+			}
+		}
+	}
+
+	function tabelas() {
+		if (!$this->verifica()) {
+			die("[ MS SQL - tabelas ]  necess&aacute;rio conectar ao DB antes.\n");
+			exit;
+		}
+		$this->buscar("SELECT name FROM sys.Tables");
+		while ($a = $this->arrayx($this->query, "NUMERIC")) {
+			$listas[] = $a[0];
+		}
+		return $listas;
+	}
+
+	function erro($a = 'desconhecido', $DB = TRUE) {
+		$msg = $nome;
+		$msg .= $a;
+		if ($DB) {
+			if (($errors = sqlsrv_errors() ) != null) {
+				$msg .= "\n";
+				foreach ($errors as $error) {
+					$msg .= "SQLSTATE: " . $error['SQLSTATE'] . "<br />\n";
+					$msg .= "code: " . $error['code'] . "<br />\n";
+					$msg .= "message: " . $error['message'] . "<br />\n";
+				}
+			}
+		}
+		die($msg);
+	}
+
 }
